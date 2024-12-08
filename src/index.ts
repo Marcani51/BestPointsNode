c7a5de7033c08fadca89e9f721878d80f4b2adb5
bithealthdenta <marcellus.denta@bithealth.co.id>
2024-12-09 00:06:50 +0700

    finishing project chapter 1

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,20 @@ import express from "express"
 import path from "path"
 import mongoose from "mongoose"
 import { Place } from "./models/place"
-
+import methodOverride from 'method-override'
 const app = express()
 
-// connect to mongoose
+// connect to mongoose mac
 
-mongoose.connect("mongodb://127.0.0.1:27017/bestpoints")
+// mongoose.connect("mongodb://127.0.0.1:27017/bestpoints")
+//     .then((result:any)=>{
+//         console.log("Succes connect to MongoDB")
+//     }).catch((err:any)=>{
+//         console.log(err)
+//     })
+
+// connect to mongoose windows
+mongoose.connect("mongodb://127.0.0.1/bestpoints")
     .then((result:any)=>{
         console.log("Succes connect to MongoDB")
     }).catch((err:any)=>{
@@ -20,6 +28,8 @@ app.set('views', path.join(__dirname, "views"))
 
 // middleware
 app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
+
 app.get('/',(req,res)=>{
     res.render('home')
 })
@@ -46,6 +56,23 @@ app.get('/places/:id',async (req,res)=>{
     const place = await Place.findById(req.params.id)
     res.render('places/show',{place})
 
+})
+
+app.get('/places/:id/edit', async(req,res)=>{
+    const place = await Place.findById(req.params.id)
+
+    res.render('places/edit',{place})
+})
+
+app.put('/places/:id',async (req,res)=>{
+    await Place.findByIdAndUpdate(req.params.id, {...req.body.place})
+
+    res.redirect("/places")
+})
+
+app.delete('/places/:id',async (req,res)=>{
+    await Place.findByIdAndDelete(req.params.id)
+    res.redirect('/places')
 })
 
 // app.get('/seed/place',async (req,res)=>{
