c3534c2a967fefa5fa238497c41998e50c66de01
Marcellus Denta Widyapramana <denta.marcellus@gmail.com>
2024-12-30 13:18:00 +0700

    editing command for deploying

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const app = express();
 // connect to mongoose mac
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/bestpoints")
+  .connect("mongodb+srv://Marcani51:marcellus.denta96@cluster0.umbob.mongodb.net/BestPoints?retryWrites=true&w=majority")
   .then((result: any) => {
     console.log("Succes connect to MongoDB");
   })
